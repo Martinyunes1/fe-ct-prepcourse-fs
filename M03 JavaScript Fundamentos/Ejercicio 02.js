f4e8bc2ce3b274d5d6ec887ec014e0b8b6a309d3
martinyunes1 <martinyunes8@gmail.com>
2023-06-29 15:12:53 -0300

    tareas modulo 4

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -2,43 +2,59 @@
 /*2️⃣ EJERCICIO 02 2️⃣*/
 
 function devolverString(string) {
-   // Debe retornar un string.
-   // Tu código:
+  // Debe retornar un string.
+  // Tu código:
+  return string;
 }
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
-   // Retorna el resultado de su suma.
-   // Tu código:
+  // Retorna el resultado de su suma.
+  // Tu código:
+  var suma = x + y;
+  return suma;
+  suma(2, 8);
 }
 
 function resta(x, y) {
-   // Retorna el resultado de la resta.
-   // Tu código:
+  // Retorna el resultado de la resta.
+  // Tu código:
+  var resta = x - y;
+  return resta;
+  resta(8 - 2);
 }
 
 function divide(x, y) {
-   // Retorna el resultado de su división.
-   // Tu código:
+  // Retorna el resultado de su división.
+  // Tu código:
+  var divide = x / y;
+  return divide;
+  divide(50 / 2);
 }
 
 function multiplica(x, y) {
-   // Retorna el resultado de su multiplicación.
-   // Tu código:
+  // Retorna el resultado de su multiplicación.
+  // Tu código:
+  var multiplica = x * y;
+  return multiplica;
+  multiplica(5 * 2);
 }
 
 function obtenerResto(x, y) {
-   // Obten el resto de la división de "x" entre "y".
-   // Tu código:
+  // Obten el resto de la división de "x" entre "y".
+  // Tu código:
+  var obtenerResto = x % y;
+  return obtenerResto;
+  obtenerResto(50 % 3);
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   devolverString,
-   suma,
-   resta,
-   divide,
-   multiplica,
-   obtenerResto,
+  devolverString,
+  suma,
+  resta,
+  divide,
+  multiplica,
+  obtenerResto,
 };
